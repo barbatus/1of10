f6c66bf7f293ab9038b1e89335a38cf7f601e0b8
alex <alex.borodach@gmail.com>
2024-10-15 13:35:49 +0200

    Fix keyframes naming

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
   darkMode: ["class"],
   content: [
@@ -14,65 +14,65 @@ export default {
   theme: {
   	extend: {
   		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
+  			lg: "var(--radius)",
+  			md: "calc(var(--radius) - 2px)",
+  			sm: "calc(var(--radius) - 4px)"
   		},
   		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
+  			background: "hsl(var(--background))",
+  			foreground: "hsl(var(--foreground))",
   			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
+  				DEFAULT: "hsl(var(--card))",
+  				foreground: "hsl(var(--card-foreground))"
   			},
   			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
+  				DEFAULT: "hsl(var(--popover))",
+  				foreground: "hsl(var(--popover-foreground))"
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  				DEFAULT: "hsl(var(--primary))",
+  				foreground: "hsl(var(--primary-foreground))"
   			},
   			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
+  				DEFAULT: "hsl(var(--secondary))",
+  				foreground: "hsl(var(--secondary-foreground))"
   			},
   			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
+  				DEFAULT: "hsl(var(--muted))",
+  				foreground: "hsl(var(--muted-foreground))"
   			},
   			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
+  				DEFAULT: "hsl(var(--accent))",
+  				foreground: "hsl(var(--accent-foreground))"
   			},
   			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
+  				DEFAULT: "hsl(var(--destructive))",
+  				foreground: "hsl(var(--destructive-foreground))"
   			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
+  			border: "hsl(var(--border))",
+  			input: "hsl(var(--input))",
+  			ring: "hsl(var(--ring))",
   			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
+  				"1": "hsl(var(--chart-1))",
+  				"2": "hsl(var(--chart-2))",
+  				"3": "hsl(var(--chart-3))",
+  				"4": "hsl(var(--chart-4))",
+  				"5": "hsl(var(--chart-5))"
   			}
   		},
       keyframes: {
-        enter: {
-          '0%': { backgroundColor: 'rgba(255, 255, 0, 1)' },
-          '100%': { backgroundColor: 'rgba(255, 255, 0, 0)' },
+        "list-enter": {
+          "0%": { backgroundColor: "rgba(255, 255, 0, 1)" },
+          "100%": { backgroundColor: "rgba(255, 255, 0, 0)" },
         },
-        exit: {
-          '0%': { opacity: 1 },
-          '100%': { opacity: 0 },
+        "list-exit": {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
         },
       },
       animation: {
-        enter: 'enter 0.5s forwards',
-        exit: 'exit 0.5s forwards',
+        enter: "list-enter 0.5s forwards",
+        exit: "list-exit 0.5s forwards",
       },
   	}
   },
